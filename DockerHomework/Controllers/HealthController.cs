619db037e5ee709962cea33e4abcbc9e1076fd0e
Aleksandr Mulgachev <a.mulgachev@urent.ru>
2023-05-15 23:15:38 +0400

    first commit

---
+++ DockerHomework/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using DockerHomework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerHomework.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public HealthModel GetHealth()
+    {
+        return new HealthModel
+        {
+            Status = "OK"
+        };
+    }
+}
---
+++ DockerHomework/Models/HealthModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DockerHomework.Models;
+
+public class HealthModel
+{
+    public string? Status { get; set; }
+}
